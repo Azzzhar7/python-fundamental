3363377a15ffc58ddde5168844a99474827366c1
Azzzhar <muhammadazhar806@gmail.com>
2025-08-01 11:14:17 +0500

    Task 2 Variables

---
+++ variables.py
@@ -0,0 +1,8 @@
+# This is a comment
+name = "NAME"     # Variable: string
+age = 25                 # Variable: integer
+height = 5.9       # Variable: float
+
+# Proper indentation
+if age > 18:
+    print(name, "is an adult.")
